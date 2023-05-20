18c8e1ccdcc827837b4fe1fd8d3edb1de095a2c6
Syed Burhan Ali Shah <50847838+CanDude@users.noreply.github.com>
2023-05-20 17:50:21 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# test
+# React Editable Table
+This is my first time to use typescript and Vite framework for this test. Hope it will work.
+
+For running command "npm run dev"
---
+++ src/EditableTable/data.ts
@@ -0,0 +1,33 @@
+export type Student = {
+  id: number
+  name: string
+  dateOfBirth: string
+  major: string
+}
+
+export const data: Student[] = [
+  {
+    id: 1111,
+    name: 'Burhan Ali',
+    dateOfBirth: '1995-01-04',
+    major: 'Computer Science',
+  },
+  {
+    id: 2222,
+    name: 'Salman Ali',
+    dateOfBirth: '1991-05-10',
+    major: 'Communications',
+  },
+  {
+    id: 3333,
+    name: 'Rahat Ali',
+    dateOfBirth: '1991-10-12',
+    major: 'Business',
+  },
+  {
+    id: 4444,
+    name: 'Rizwan Ali',
+    dateOfBirth: '2000-09-24',
+    major: 'Psychology',
+  },
+]
